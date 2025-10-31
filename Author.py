da792403aac53859001c2c283d56f0861b8d0286
pkxxx <pkxxx@spoko.pl>
2025-10-31 16:41:29 +0100

    obiektowo

--- Author.py
+++ Author.py
@@ -1,27 +1,49 @@
 from requests import get
 
-def get_authors(search:str=None):
-    authors =[]
-    query = get('https://wolnelektury.pl/api/authors/')
-    for author_id, author in enumerate(query.json(), start=1):
-        if search is None or search in author['name']:
-            #todo: convert author to object
 
-            authors.append({
-                'author_id': author_id,
-                'name': author.get('name'),
-                'api_books_url': author.get('href') + 'books'
-            })
-    return authors
+class Author:
+    def __init__(self, author_id: int, name: str, slug: str, api_books_url: str):
+        self.author_id = author_id
+        self.name = name
+        self.slug = slug
+        self.api_books_url = api_books_url
 
-authors = get_authors('Adam')
-for author in authors:
-    # todo create method to display author as string
-    print(f'{author.get("author_id")}. {author.get("name")}')
+    def __str__(self):
+        return f'{self.author_id}. {self.name}'
 
-# print(authors)
-search_author_id = int(input('Którego autora książki chcesz zobaczyć? '))
-found_author = next(filter(lambda auth: auth.get('author_id') == search_author_id, authors))
 
-for book in get(found_author.get('api_books_url')).json():
-    print(f'- {book.get("title")}')
+class WolneLekturyAdapter:
+    API_URL = 'https://wolnelektury.pl/api/'
+
+    def get_authors(self, search: str = None):
+        authors = []
+        query = get(self.API_URL + 'authors/')
+        for author_id, author in enumerate(query.json(), start=1):
+            if search is None or search in author['name']:
+                authors.append(Author(
+                    author_id=author_id,
+                    name=author.get('name'),
+                    slug=author.get('slug'),
+                    api_books_url=author.get('href') + 'books'
+                ))
+        return authors
+
+    def get_books(self, author_slug: str):
+        return get(f'{self.API_URL}authors/{author_slug}/books').json()
+
+class Application:
+    def main(self):
+        adapter = WolneLekturyAdapter()
+        authors = adapter.get_authors('Adam')
+        for author in authors:
+            print(author)
+
+        search_author_id = int(input('Którego autora książki chcesz zobaczyć? '))
+        found_author = next(filter(lambda auth: auth.author_id == search_author_id, authors))
+
+        for book in adapter.get_books(found_author.slug):
+            print(f'- {book["title"]}')
+
+if __name__ == '__main__':
+    app = Application()
+    app.main()
